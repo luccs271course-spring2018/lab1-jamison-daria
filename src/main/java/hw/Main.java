16f40127b73f655b3303b081fe047e71ef2323a7
Jamison Dale <jdale1@luc.edu>
2017-09-14 01:33:00 +0000

    test commit

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,14 +1,34 @@
 package hw;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
-  public static void main(final String[] args) {
-    System.out.println("args = " + Arrays.asList(args));
-    final HelloWorld instance = new HelloWorld();
-    System.out.println(instance.getMessage());
-    System.out.println(instance.getYear());
-    System.out.println("bye for now");
+  public static void main(String[] args) {
+    System.out.println("Please input a number: ");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println("We will now determine fizz buzz");
+    Main.fizzbuzz(n);
+  }
+
+  public static String[] fizzbuzz(int n) {
+    if (n >= 1) {
+      String[] list = new String[n+1];
+      for (int i = 1; i <= n; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+          list[i] = "fizz buzz";
+        } else if (i % 5 == 0) {
+          list[i] = "buzz";
+        } else if (i % 3 == 0) {
+          list[i] = "fizz";
+        } else {
+          list[i] = String.valueOf(i);
+        }
+      }
+      return list;
+    }
+    return null;
   }
 }
